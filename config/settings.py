62c42c992ffc28b38e6c2c246ad447f5c5b0de37
Yash <sawantyash9559@gmail.com>
2026-03-23 09:49:52 +0530

    fix login loop on render

--- config/settings.py
+++ config/settings.py
@@ -154,5 +154,9 @@ CSRF_TRUSTED_ORIGINS = [
     "https://seovista.onrender.com"
 ]
 
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = not DEBUG
+CSRF_COOKIE_SECURE = not DEBUG
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
